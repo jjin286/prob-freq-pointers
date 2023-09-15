25c90968bac10b1c91c4caec06981ffa1a6a5ce0
JaeWon Jin <j1jin286@gmail.com>
2023-09-15 15:53:44 -0700

    completed can-contruct-word.js and average-pair.js

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,41 @@
+"use strict";
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+/**
+ * Find pair/pairs of numbers that average to target number
+ * Input: sorted array of numbers, target number
+ * Output: Boolean
+ */
+function averagePair(nums, targetAvg) {
+  // Multiple pointers
+  //check empty array, return false
+  //left index and right index
+  //check if index values average to target number return true
+  //if average of pair is < target value, move left index
+  //if average of pair is > target value, move right index
+// averagePair([1, 2, 3], 2.5);         // true
+// averagePair([3, 3, 6, 12, 19], 8);   // false
+// averagePair([1, 2, 3], 2);           // true
+// averagePair([], 4);
+
+  if(nums.length === 0) return false;
+
+  let leftIdx = 0;
+  let rightIdx = nums.length - 1;
+
+  while(leftIdx < rightIdx){
+    let average = (nums[leftIdx] + nums[rightIdx]) / 2;
+
+    if(average === targetAvg) return true;
+
+    if(average < targetAvg){
+      leftIdx++;
+    }
+
+    if(average > targetAvg){
+      rightIdx--;
+    }
+  }
+
+  return false;
 }
 
--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,43 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+/**
+ * Checks if word can be constructed using given letters
+ * inputs: word, letter
+ * output: boolean
+ */
+function canConstructWord(word, letter) {
+  // Create frequency helper function
+
+  // {a:2} {a:1, b:1, c:1}
+  // check if word freq key is in letter freq; return false
+  // check if word freq value is equal/less than in letter freq; return false;
+  // return true;
+  const wordFreq = frequencyCounter(word);
+  const letterFreq = frequencyCounter(letter);
+  
+  console.log("Word Freq: ", wordFreq);
+  console.log("Letter Freq: ", letterFreq);
+
+  for(let char in wordFreq){
+    if(!(char in letterFreq)) return false;
+
+    if(wordFreq[char] > letterFreq[char]) return false;
+  }
+
+  return true;
+}
+
+/**
+ * Creates an object containg char frequency, input string, output object
+ */
+function frequencyCounter(str) {
+  const counter = {};
+
+  for(let char of str){
+    const curr = counter[char] || 0;
+    counter[char] = curr + 1;
+  }
+
+  return counter;
 }
