83b5044eef2f024c96fab17ea424ef35ad53ca35
Silu Hu <erinh816@gmail.com>
2023-09-15 19:30:52 -0400

    finish sameFrequency and twoArrayObject

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,36 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+/**
+ this function takes 2 positive integers to check if they have the same digit frequency
+ input: num1, num2
+ output: boolean
+ */
+
+function sameFrequency(num1, num2) {
+  //set up the frequency function out of this function
+  //get both inputs frequency objects
+  const freqsNum1 = getFrequencyCounter(num1);
+  const freqsNum2 = getFrequencyCounter(num2);
+
+  //loop over num1's frequency object using for in loop
+  //check if keys from num1obj are not in num2obj, return false
+  //check if values from num1obj don't match values in num2 obj, return false
+  //return true
+  for (let key in freqsNum1) {
+    if (!freqsNum2[key]) return false;
+    if (freqsNum1[key] !== freqsNum2[key]) return false;
+  }
+
+  return true;
+}
+
+function getFrequencyCounter(items) {
+  const freqs = {};
+  const stringItems = '' + items;
+
+  for (let item of stringItems) {
+    const currItem = freqs[item] || 0;
+    freqs[item] = currItem + 1;
+  }
+  return freqs;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,30 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+
+/**
+ function twoArrayObject takes 2 arrays, might be different lengths, return an object with keys from first array and values from second array
+ and values from second array
+ input:keys, values
+ output:object
+ */
+
+function twoArrayObject(keys, values) {
+  //declare a const variable as the final object
+  const mergedObj = {};
+
+
+  //loop over keys array to access all elements as keys - index for loop
+  //use the index of the keys array to access elements from values as the values of the obj property
+  //create object[element of keys] = values[i]
+  //if values at index is undefined, reassign object[element of keys] to null
+  for (let i = 0; i < keys.length; i++) {
+
+    mergedObj[keys[i]] = values[i];
+
+    if (values[i] === undefined) {
+      mergedObj[keys[i]] = null;
+    }
+  }
+  //return final object
+  return mergedObj;
 }
