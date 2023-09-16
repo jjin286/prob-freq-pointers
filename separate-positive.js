e176122dcfd8d3de22c99ceb7118c2f9505fc152
JaeWon Jin <j1jin286@gmail.com>
2023-09-15 17:03:04 -0700

    separate-positive.js in progress

--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,33 @@
-// add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+"use strict";
+
+/**
+ * Separate positive from negative numbers in an array
+ * Input: non-zero numbers
+ * Output: Original array
+ */
+function separatePositive(unseparated) {
+  //leftidx rightidx
+  //while leftidx < rightidx
+  //if leftidx > 0 leftidx++
+  //if rightidx < 0 rightidx--
+
+  //if leftidx < 0 and rightidx > 0
+  //swap using array destructuring
+  //return
+  let leftIdx = 0;
+  let rightIdx = unseparated.length - 1;
+
+  while(leftIdx < rightIdx){
+    if(unseparated[leftIdx] > 0){
+      leftIdx++;
+    }
+
+    if(unseparated[rightIdx] > 0){
+      rightIdx--;
+    }
+
+    if(unseparated[leftIdx] < 0 && unseparated[rightIdx] > 0){
+      let [leftIdx, ...other ,rightIdx] = unseparated;
+    }
+  }
 }
